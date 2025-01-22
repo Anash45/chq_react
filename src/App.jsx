a24fa04c85fd59b53a5c28832db54451ff25aedd
Syed Anas Bukhari <anas14529@gmail.com>
2025-01-22 06:31:22 +0500

    Done.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App bg-red-500 h-[100vh] flex flex-col justify-center items-center">
+      <p className="text-center text-xl font-bold text-white">Hello React</p>
+    </div>
+  );
+}
+
+export default App;
